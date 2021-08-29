b042754bc13f751223501efc2b8320288bcc1a94
Rafael Rocha <rafaelsantoscv_@hotmail.com>
2021-08-28 22:49:46 -0300

    Observer design pattern, OCP SOLID principal, DeliveRevision

--- backend/src/main/java/com/rafael/dslearndbs/services/DeliverService.java
+++ backend/src/main/java/com/rafael/dslearndbs/services/DeliverService.java
@@ -2,16 +2,22 @@ package com.rafael.dslearndbs.services;
 
 import com.rafael.dslearndbs.dto.DeliverRevisionDTO;
 import com.rafael.dslearndbs.entities.Deliver;
+import com.rafael.dslearndbs.observers.DeliverRevisionObserver;
 import com.rafael.dslearndbs.repositories.DeliverRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 @Service
 public class DeliverService {
 
     @Autowired
     private DeliverRepository deliverRepository;
+
+    private Set<DeliverRevisionObserver> deliverRevisionObservers = new LinkedHashSet<>();
 
     @Transactional
     public void saveRevision(Long id, DeliverRevisionDTO dto) {
@@ -21,5 +27,12 @@ public class DeliverService {
         deliver.setCorrectCount(dto.getCorrectCount());
 
         deliverRepository.save(deliver);
+        for (DeliverRevisionObserver observer: deliverRevisionObservers) {
+            observer.onSaveRevision(deliver);
+        }
+    }
+
+    public void subscribeDeliverRevisionObserver(DeliverRevisionObserver observer) {
+        deliverRevisionObservers.add(observer);
     }
 }
--- backend/src/main/java/com/rafael/dslearndbs/services/NotificationService.java
+++ backend/src/main/java/com/rafael/dslearndbs/services/NotificationService.java
@@ -1,16 +1,22 @@
 package com.rafael.dslearndbs.services;
 
 import com.rafael.dslearndbs.dto.NotificationDTO;
+import com.rafael.dslearndbs.entities.Deliver;
 import com.rafael.dslearndbs.entities.Notification;
 import com.rafael.dslearndbs.entities.User;
+import com.rafael.dslearndbs.observers.DeliverRevisionObserver;
 import com.rafael.dslearndbs.repositories.NotificationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.PostConstruct;
+import java.time.Instant;
 
 @Service
-public class NotificationService {
+public class NotificationService implements DeliverRevisionObserver {
 
     @Autowired
     private NotificationRepository notificationRepository;
@@ -18,9 +24,38 @@ public class NotificationService {
     @Autowired
     private AuthService authService;
 
+    @Autowired
+    private DeliverService deliverService;
+
+    @PostConstruct
+    private void initialize() {
+        deliverService.subscribeDeliverRevisionObserver(this);
+    }
+
+    @Transactional(readOnly = true)
     public Page<NotificationDTO> notificationForCurrentUser(boolean unreadOnly, Pageable pageable) {
         User user = authService.authenticated();
         Page<Notification> page = notificationRepository.find(user, unreadOnly, pageable);
         return page.map(NotificationDTO::new);
+    }
+
+    @Transactional
+    public void saveDeliverNotification(Deliver deliver) {
+        Long sectionId = deliver.getLesson().getSection().getId();
+        Long resourceId = deliver.getLesson().getSection().getResource().getId();
+        Long offerId = deliver.getLesson().getSection().getResource().getOffer().getId();
+
+        String route = "/offers/" + offerId + "/resources/" + resourceId + "/sections/" + sectionId;
+        String text = deliver.getFeedback();
+        Instant moment = Instant.now();
+        User user = deliver.getEnrollment().getStudent();
+
+        Notification notification = new Notification(null, text, moment, false, route, user);
+        notificationRepository.save(notification);
+    }
+
+    @Override
+    public void onSaveRevision(Deliver deliver) {
+        saveDeliverNotification(deliver);
     }
 }
